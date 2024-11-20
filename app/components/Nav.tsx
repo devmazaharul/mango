7cbd6bdcd1bfd0e394caa1a2c2fbb2aa633785b6
mazaharul islam <expertmazahrul@gmail.com>
2024-11-20 20:38:09 +0600

    upload

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -5,28 +5,45 @@ import { IoBagAdd } from "react-icons/io5";
 
 export default function Nav() {
   return (
-        
     <div>
       <div className="flex items-center  justify-between px-8 py-3  text-gray-800 ">
         <div>
-        <Link className="text-xl font-semibold" href={'/'}>Mangoose</Link>
+          <Link className="text-xl font-semibold" href={"/"}>
+            Mangoose
+          </Link>
         </div>
         <div>
-        <div className="hidden md:block ">
-        <ul className="md:flex items-center gap-3">
-            <li><Link href={'/'}>হোম</Link></li>
-            <li><Link href={'/catagory'}>ক্যাটাগরি</Link></li>
-            <li><Link href={'/session'}>সিজন </Link></li>
-            <li><Link href={'/customer'}>কাস্টমার  </Link></li>
-          </ul>
-        </div>
+          <div className="hidden md:block ">
+            <ul className="md:flex items-center gap-3">
+              <li>
+                <Link href={"/"}>হোম</Link>
+              </li>
+              <li>
+                <Link href={"/catagory"}>ক্যাটাগরি</Link>
+              </li>
+              <li>
+                <Link href={"/session"}>সিজন </Link>
+              </li>
+              <li>
+                <Link href={"/customer"}>কাস্টমার </Link>
+              </li>
+              <li>
+                <Link href={"/store"}>Store </Link>
+              </li>
+            </ul>
+          </div>
         </div>
         <div className="flex items-center gap-8">
-          <IoBagAdd className="text-3xl"/>
-          <Image className="rounded-full" src={'/mangoose.png'} alt="site_image" width={60} height={50}/>
+          <IoBagAdd className="text-3xl" />
+          <Image
+            className="rounded-full"
+            src={"/mangoose.png"}
+            alt="site_image"
+            width={60}
+            height={50}
+          />
         </div>
       </div>
-    
     </div>
   );
 }
---
+++ app/store/page.tsx
@@ -0,0 +1,29 @@
+import { productsArr } from "@/data/Mangodata";
+
+export default function page() {
+  return (
+    <div className="grid md:grid-cols-2  items-center gap-3">
+      {productsArr.map((items, index) => (
+        <div key={index} className=" rounded-md shadow-xl p-2">
+          <h1 className="text-center text-xl font-semibold">
+            {items.catagory.title}
+          </h1>
+          <div
+            
+            className="grid gap-2 grid-cols-2 md:grid-cols-3  py-3 px-2 "
+          >
+            {items.catagory.productsData.map((list, i) => (
+              <div key={i} className="border p-2 rounded-md shadow-lg">
+                <h1 className="text-2xl text-green-500 py-2">{list.name}</h1>
+                <h1>{list.desc}</h1>
+                <h1 className="text-emerald-500 bg-emerald-100 p-2 rounded-md text-center w-fit mx-auto">
+                  {list.price} Taka
+                </h1>
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
